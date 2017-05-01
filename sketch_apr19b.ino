852202869cd6a5b981990789516c2e0a75002846
Isabella Reiff <izzyreiff@gmail.com>
2017-05-01 15:10:24 -0700

    Update sketch_apr19b.ino

--- sketch_apr19b.ino
+++ sketch_apr19b.ino
@@ -1,23 +1,24 @@
 #include <EYW.h>
 
-EYW::Altimeter myalt;
-float drop_height = 0;
-float current_height=0;
+EYW::Altimeter a;
+
+float drop_height = 0; //input initial drop height
+float current_height = 0; //input initial drop height
+
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-myalt.begin();
-myalt.calibrate(100); //takes first 100 measurements as calibration
-myalt.alarm();
+a.begin();
+a.calibrate(100); //takes first 100 measurements as calibration
+a.alarm();
 }
 
 void loop() {
-current_height = myalt.getHeightAvg (20); //takes the average of 20 heights to get current height
+current_height = a.getHeightAvg (20);//takes the average of 20 heights to get current height
 Serial.print("Current Height: "+current_height+"");
-//Serial.println(current_height);
 
 if (current_height > drop_height){
-  myalt.alarm(6,2000,500);
+  a.alarm(6,2000,500);
   //intiates the drop mechanism
 }
 }
